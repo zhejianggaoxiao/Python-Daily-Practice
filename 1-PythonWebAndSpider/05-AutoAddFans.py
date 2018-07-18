d6e1b8a213bb6126387b1a1aa9c7c40f14134e14
gaox <zhejiangblue@hotmail.com>
2018-07-18 10:39:31 +0800

    finish 04

--- 1-PythonWebAndSpider/05-AutoAddFans.py
+++ 1-PythonWebAndSpider/05-AutoAddFans.py
@@ -1,7 +1,7 @@
 """
 -------------------------------------------------
    File Name：     05-AutoAddFans
-   Description :
+   Description :   在知乎上自动互粉
    Author :       gaox
    date：          7/18/18
 -------------------------------------------------
@@ -10,3 +10,33 @@
 -------------------------------------------------
 """
 __author__ = 'gaox'
+
+# cookie 登录
+
+from selenium import webdriver
+import time
+
+def start_chrome():
+    driver = webdriver.Chrome('../0-Additionals/chromedriver')
+    driver.start_client()
+    return driver
+
+def find_strangers():
+    btn_sel = 'div.ContentItem-extra > button.Button--blue'
+    elms = driver.find_elements_by_css_selector(btn_sel)
+    return elms
+
+
+username = 'None'
+url = 'www.zhihu.com'
+follower_url = 'http://zhihu.com/people/{}/followers'.format(username)
+driver = start_chrome()
+driver.get(url)
+time.sleep(20)  # wait for log in
+driver.get(follower_url)
+time.sleep(6)   # wait for loading
+
+strangers = find_strangers()
+for s in strangers:
+    s.click()       # 模拟js的单击操作
+
